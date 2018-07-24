9c0ca0cb615de86365746a71774ca95b4e3a8169
romanosen <romanosen@gmail.com>
2018-07-24 16:06:54 +0300

    Finished hql query example for deleting data

--- src/com/luv2code/hibernate/demo/stddemo/DeleteStudentDemo.java
+++ src/com/luv2code/hibernate/demo/stddemo/DeleteStudentDemo.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class UpdateStudentDemo {
+public class DeleteStudentDemo {
     public static void main(String[] args) {
         //create session factory
         SessionFactory factory= (SessionFactory) new Configuration()
@@ -25,24 +25,20 @@ public class UpdateStudentDemo {
 
             //retrieve student based on id: primary key
             System.out.println("\nGetting student with id: "+ studentId);
+            //delete the student
+//            Student myStudent=session.get(Student.class,studentId);
+//            System.out.println("Student "+myStudent+" deleted");
+//            session.delete(myStudent);
 
-            Student student1=session.get(Student.class,studentId);
-
-            System.out.println("Get complete: "+student1);
-
-            System.out.println("Updating student");
-            student1.setFirstName("Scooby");
-
-            //commit the transaction
-            session.getTransaction().commit();
+            //delete the student id=2
+            session.createQuery("delete from Student where id=2").executeUpdate();
+            System.out.println("Student id=2  deleted");
 
 
-            session=factory.getCurrentSession();
-            session.beginTransaction();
 
-            //update email for all users
-            System.out.println("update the email for all users");
-            session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+
+
+
 
 
             session.getTransaction().commit();
